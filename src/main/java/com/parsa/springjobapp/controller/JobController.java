389e6d48620d0817ae3dcfed34ee88f44c3d6220
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-28 16:56:50 +0330

    add JobService integration and viewAllJobs mapping in JobController

--- src/main/java/com/parsa/springjobapp/controller/JobController.java
+++ src/main/java/com/parsa/springjobapp/controller/JobController.java
@@ -2,12 +2,19 @@ package com.parsa.springjobapp.controller;
 
 
 import com.parsa.springjobapp.model.jobPost;
+import com.parsa.springjobapp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class JobController {
+
+    @Autowired
+    private JobService jobService;
+
+
     @RequestMapping({"/", "home"})
     public String home(){
         return "home";
@@ -21,6 +28,12 @@ public class JobController {
 
     @RequestMapping(value = "/handleForm", method = RequestMethod.POST)
     public String handleForm(jobPost jobPost){
+        jobService.addJob(jobPost);
         return "success";
+    }
+
+    @RequestMapping(value = "/viewalljobs", method = RequestMethod.GET)
+    public String viewAllJobs(){
+        return "viewalljobs";
     }
 }
