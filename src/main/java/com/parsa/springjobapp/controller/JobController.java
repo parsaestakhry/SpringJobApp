7708f50740ccd7d094bcd77ab142317b78979c15
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-03-28 16:35:10 +0330

    add handleForm method to JobController for job post submission

--- src/main/java/com/parsa/springjobapp/controller/JobController.java
+++ src/main/java/com/parsa/springjobapp/controller/JobController.java
@@ -1,8 +1,10 @@
 package com.parsa.springjobapp.controller;
 
 
+import com.parsa.springjobapp.model.jobPost;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class JobController {
@@ -14,5 +16,11 @@ public class JobController {
     @RequestMapping("/addjob")
     public String addJob(){
         return "addjob";
+    }
+
+
+    @RequestMapping(value = "/handleForm", method = RequestMethod.POST)
+    public String handleForm(jobPost jobPost){
+        return "success";
     }
 }
